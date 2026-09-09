7156545ed81393e695a7b31200793693521b7a49
maslazu-96 <finacc.unsinn@gmail.com>
2026-09-09 15:52:42 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='vendor-order-unsinn-v3';
-const ASSETS=['/','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET'||new URL(e.request.url).pathname.startsWith('/api/'))return;
-  e.respondWith(fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))));
-});
